0864750d46701eddba7ae3cbd9fab1195f21ffc5
adityayadav71 <adityayadav1088@gmail.com>
2023-04-09 09:04:05 +0530

    return hints from api call

--- src/api.js
+++ src/api.js
@@ -1,6 +1,15 @@
 export const fetchWordDetails = async (word) => {
   const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
   const data = await res.json();
-  const audioFiles = data[0].phonetics.filter((phonetic) => phonetic.audio !== "");
-  return audioFiles[0].audio;
+  const hints = data[0]?.meanings?.map((meaning) => {
+    return {
+      definition: meaning.definitions.filter((def) => {
+        const definition = def.definition.split(" ").map((word) => word.toLowerCase());
+        return !definition.includes(word.toLowerCase());
+      })[0].definition,
+      partOfSpeech: meaning.partOfSpeech,
+    };
+  });
+  const audioFiles = data[0]?.phonetics?.filter((phonetic) => phonetic.audio !== "");
+  return { audio: audioFiles[0]?.audio, hints };
 };
